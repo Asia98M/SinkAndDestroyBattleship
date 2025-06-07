729e47b92ec8a920ea64ec865169fb925d96bd7a
marce <marcel.senn@students.fhnw.ch>
2025-06-07 10:28:34 +0200

    reworked server message

--- app/src/main/java/com/example/sinkanddestroybattleship/data/models/GameModels.kt
+++ app/src/main/java/com/example/sinkanddestroybattleship/data/models/GameModels.kt
@@ -110,10 +110,11 @@ data class ErrorResponse(
     val Error: String? = null
 )
 
+
 enum class ShipType(val size: Int) {
     Carrier(5),
     Battleship(4),
     Destroyer(3),
     Submarine(3),
     PatrolBoat(2)
-} 
+}
